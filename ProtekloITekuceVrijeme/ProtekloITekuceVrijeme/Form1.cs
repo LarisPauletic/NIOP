a211de0a7884a8898b866e3ffbde3967ff81cb51
LarisPauletic <58471911+LarisPauletic@users.noreply.github.com>
2021-10-04 09:57:51 +0200

    Add files via upload

---
+++ ProtekloITekuceVrijeme/ProtekloITekuceVrijeme/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProtekloITekuceVrijeme
+{
+    public partial class Form1 : Form
+    {
+        List<Test> listaTest = new List<Test>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Test test = new Test(textBox1.Text);
+            listaTest.Add(test);
+            textBox2.Clear();
+            //textBox2.AppendText(test.ToString());
+            foreach ( Test tst in listaTest)
+            {
+                textBox2.AppendText(tst.ToString() + "\r\n");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 frm2 = new Form2();
+
+        }
+    }
+}
---
+++ ProtekloITekuceVrijeme/ProtekloITekuceVrijeme/Form2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProtekloITekuceVrijeme
+{
+    public partial class Form2 : Form
+    {
+
+        List<Test> lista = new List<Test>();
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public Form2(List<Test> lista)
+        {
+            InitializeComponent();
+        }
+
+        internal List<Test> Lista { get => lista; set => lista = value; }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
